0916e491078d262fccc55170dd8f7862c6c52c32
Átila <atilasilva3011@gmail.com>
2023-10-08 16:24:21 -0300

    desafio de lógica do felpao

---
+++ index.js
@@ -0,0 +1,23 @@
+let heroi = ["Dean Winchester", 5.500];
+
+if(heroi[1]<= 1.000){
+    console.log("Héroi de nome " + heroi[0] + "está no nível ferro" );
+}else if (heroi[1]>=1.001 && heroi[1]<=2.000){
+    console.log("Héroi de nome " + heroi[0] + "está no nível Bronze" );
+}else if (heroi[1]>=2.001 && heroi[1]<=5.000){
+    console.log("Héroi de nome " + heroi[0] + "está no nível Prata" );
+}else if(heroi[1]>=5.001 && heroi[1]<=6.000){
+    console.log("Héroi de nome " + heroi[0] + "está no nível Audaz" );
+}else if (heroi[1]>=6.001 && heroi[1]<=7.000) {
+    console.log("Héroi de nome " + heroi[0] + "está no nível Ouro" );
+}else if(heroi[1]>=7.001 && heroi[1]<=8.000){
+    console.log("Héroi de nome " + heroi[0] + "está no nível Platina" );
+}else if(heroi[1]>8.001 && heroi[1]<9.000){
+    console.log("Héroi de nome "+ heroi[0]+ " está no nível Ascendente");
+}else if(heroi[1]>9.001 && heroi[1]<10.000){
+    console.log ("Héroi de nome"+ heroi [0]+" está no nível Imortal");  
+}else if(heroi[1]>=10.000){
+    console.log("Héroi de nome "+ heroi [0] +" está no nível Radiante") ;
+}
+
+
